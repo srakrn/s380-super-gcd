2e91750c338c35c7fd18e3cc6527d37df4e94f14
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2022-08-11 11:53:21 +0700

    Make all tkinter items class objects

--- super_gcd/gui.py
+++ super_gcd/gui.py
@@ -5,31 +5,31 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        greeting = tk.Label(text="Super GCD application!", font=("Arial", 16))
+        self.greeting = tk.Label(text="Super GCD application!", font=("Arial", 16))
 
-        frame = tk.Frame(self.parent)
+        self.frame = tk.Frame(self.parent)
 
-        label_inp_1 = tk.Label(master=frame, text="Number 1:")
-        entry_inp_1 = tk.Entry(master=frame, width=20)
-        label_inp_1.grid(row=0, column=0)
-        entry_inp_1.grid(row=0, column=1)
+        self.label_inp_1 = tk.Label(master=self.frame, text="Number 1:")
+        self.entry_inp_1 = tk.Entry(master=self.frame, width=20)
+        self.label_inp_1.grid(row=0, column=0)
+        self.entry_inp_1.grid(row=0, column=1)
 
 
-        label_inp_2 = tk.Label(master=frame, text="Number 2:")
-        entry_inp_2 = tk.Entry(master=frame, width=20)
-        label_inp_2.grid(row=1, column=0)
-        entry_inp_2.grid(row=1, column=1)
+        self.label_inp_2 = tk.Label(master=self.frame, text="Number 2:")
+        self.entry_inp_2 = tk.Entry(master=self.frame, width=20)
+        self.label_inp_2.grid(row=1, column=0)
+        self.entry_inp_2.grid(row=1, column=1)
 
-        spacer = tk.Label(master=frame, text="")
-        spacer.grid(row=3, column=0)
+        self.spacer = tk.Label(master=self.frame, text="")
+        self.spacer.grid(row=3, column=0)
 
-        btn_submit = tk.Button(master=frame, text="GCD \N{RIGHTWARDS BLACK ARROW}")
-        label_result = tk.Label(master=frame, text="")
-        btn_submit.grid(row=4, column=0, sticky="nesw")
-        label_result.grid(row=4, column=1, sticky="nesw")
+        self.btn_submit = tk.Button(master=self.frame, text="GCD \N{RIGHTWARDS BLACK ARROW}")
+        self.label_result = tk.Label(master=self.frame, text="")
+        self.btn_submit.grid(row=4, column=0, sticky="nesw")
+        self.label_result.grid(row=4, column=1, sticky="nesw")
 
-        greeting.pack()
-        frame.pack()
+        self.greeting.pack()
+        self.frame.pack()
 
 def main():
     root = tk.Tk()
